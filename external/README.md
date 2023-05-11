028b4b6e78ceef8f7d6e1d5e5d466ecaa60884ec
Naqeeb Rehman <naq.rehman@gmail.com>
2023-05-11 19:30:14 -0400

    pstreams

---
+++ external/README.md
@@ -0,0 +1,5 @@
+# External Tools
+
+- [CLI11](https://github.com/CLIUtils/CLI11) - A command line parser for C++11 and beyond that provides a rich feature set with a simple and intuitive interface.
+
+- [pstreams](https://pstreams.sourceforge.net) - Allows you to run another program from your C++ application and to transfer data between the two programs similar to shell pipelines.
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
 #include <string>
 
+#include <CLI/App.hpp> // CLI library is used for getting command line arguments
+#include <CLI/Config.hpp>
+#include <CLI/Formatter.hpp>
+
+#include <array>
+#include <cstdio>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <string>
+
+std::string exec(const char* cmd) {
+    std::array<char, 128> buffer;
+    std::string result;
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+    if (!pipe) {
+        throw std::runtime_error("popen() failed!");
+    }
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+        result += buffer.data();
+    }
+    return result;
+}
+
 using namespace std;
 
-int main() { cout << "Creating cpp project..." << endl; }
+int main() {
+    /**
+     * Set up CLI app and parse arguments into variables
+     */
+    CLI::App app("Utility program for quickly creating C++ projects");
+
+    CLI11_PARSE(app);
+
+    cout << exec("git statuss") << endl;
+}
